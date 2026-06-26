058e1d6fc3a87c6b051d6da2025ceb68047695a3
Feydral <lian.hable.feathertech@gmail.com>
2026-06-26 17:32:55 +0200

    feat(input): create a better input system

--- src/input.rs
+++ src/input.rs
@@ -1,6 +1,10 @@
 #![allow(dead_code)]
-
-use crossterm::event::{self, Event, KeyCode, KeyEvent};
+use crossterm::event::{
+    self, Event, KeyCode, KeyEvent, KeyEventKind,
+    PopKeyboardEnhancementFlags,
+};
+use crossterm::execute;
+use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
 use std::collections::HashSet;
 use std::time::Duration;
 
@@ -8,41 +12,58 @@ pub struct Input {
     held_keys: HashSet<KeyCode>,
     down_keys: HashSet<KeyCode>,
     up_keys: HashSet<KeyCode>,
+    kitty_enabled: bool,
 }
 
 impl Input {
     pub fn new() -> Self {
+        enable_raw_mode().unwrap();
+
+        #[cfg(target_os = "windows")]
+        let kitty_enabled = false;
+
+        #[cfg(not(target_os = "windows"))]
+        let kitty_enabled = {
+            assert!(
+                matches!(supports_keyboard_enhancement(), Ok(true)),
+                "Terminal does not support kitty keyboard protocol"
+            );
+            execute!(
+                std::io::stdout(),
+                PushKeyboardEnhancementFlags(KeyboardEnhancementFlags::REPORT_EVENT_TYPES)
+            )
+            .unwrap();
+            true
+        };
+
         Self {
             held_keys: HashSet::new(),
             down_keys: HashSet::new(),
             up_keys: HashSet::new(),
+            kitty_enabled,
         }
     }
 
     pub fn update(&mut self) -> std::io::Result<()> {
         self.down_keys.clear();
         self.up_keys.clear();
-
         while event::poll(Duration::from_millis(0))? {
             if let Event::Key(KeyEvent { code, kind, .. }) = event::read()? {
                 match kind {
-                    crossterm::event::KeyEventKind::Press => {
-                        if !self.held_keys.contains(&code) {
+                    KeyEventKind::Press => {
+                        if self.held_keys.insert(code) {
                             self.down_keys.insert(code);
-                            self.held_keys.insert(code);
                         }
                     }
-                    crossterm::event::KeyEventKind::Release => {
-                        if self.held_keys.contains(&code) {
+                    KeyEventKind::Release => {
+                        if self.held_keys.remove(&code) {
                             self.up_keys.insert(code);
-                            self.held_keys.remove(&code);
                         }
                     }
                     _ => {}
                 }
             }
         }
-
         Ok(())
     }
 
@@ -56,5 +77,14 @@ impl Input {
 
     pub fn is_key_up(&self, key: KeyCode) -> bool {
         self.up_keys.contains(&key)
+    }
+}
+
+impl Drop for Input {
+    fn drop(&mut self) {
+        if self.kitty_enabled {
+            let _ = execute!(std::io::stdout(), PopKeyboardEnhancementFlags);
+        }
+        disable_raw_mode().unwrap();
     }
 }
